ab45caa19a837a1905f5d316d61d60cd035723b4
Aleksandr <martynenko2sa@hotmail.com>
2023-10-02 09:53:03 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aleksandr Martynenko on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
